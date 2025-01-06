2f06773a7d084fd4366074987d99bf5579aa5c09
Andrey Essaulov <andrey.essaulov@outlook.com>
2025-01-06 12:31:15 +0200

    Wezterm more contrast colorscheme"

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Kanagawa Dragon (Gogh)"
+config.color_scheme = "Kanagawa (Gogh)"
 config.font_size = 16
 config.window_background_opacity = 0.95
 
