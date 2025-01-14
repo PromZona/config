7208b5e503b419259c068e02888e93d0406883d0
Andrey Essaulov <andrey.essaulov@outlook.com>
2025-01-14 16:19:53 +0200

    wezterm iosevka font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,11 +4,17 @@ local config = wezterm.config_builder()
 local is_windows <const> = wezterm.target_triple:find("windows") ~= nil
 local git = os.getenv("GIT")
 
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "s3r0 modified (terminal.sexy)"
+config.font = wezterm.font("Iosevka Slab", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font_size = 16
-config.window_background_opacity = 0.95
+config.window_background_opacity = 1
 
 if is_windows then
 	config.default_prog = { git .. "/bin/bash" }
 end
+
+if not is_windows then
+	config.enable_wayland = false
+end
+
 return config
